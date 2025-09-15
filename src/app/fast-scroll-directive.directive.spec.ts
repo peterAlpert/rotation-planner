d0491337bfa9dad5472033cc2458048278886a25
Peter Kameel <peterkameel.93@gmail.com>
2025-09-15 23:06:39 +0300

    ...

---
+++ src/app/fast-scroll-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FastScrollDirectiveDirective } from './fast-scroll-directive.directive';
+
+describe('FastScrollDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FastScrollDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/fast-scroll-directive.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appFastScrollDirective]',
+  standalone: true
+})
+export class FastScrollDirectiveDirective {
+
+  constructor() { }
+
+}
